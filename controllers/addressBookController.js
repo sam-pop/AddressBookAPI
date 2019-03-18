e7f0e38c54e4221330b83e07b560e89846705268
Sam Pop <poplovitch.samuel@gmail.com>
2019-03-18 15:49:56 -0400

    controller updated (methods added)

--- controllers/addressBookController.js
+++ controllers/addressBookController.js
@@ -1,1 +1,31 @@
-const db = require("../models");
+const Contact = require("../models");
+const db = require("../db");
+
+const indexName = "addressbook";
+const documentType = "contacts";
+
+module.exports = {
+  getAllContacts: function(pageSize, pageOffset, query) {},
+  getContact: function(name) {
+    db.get({
+      index: indexName,
+      type: documentType,
+      id: name
+    });
+  },
+  addContact: function(contact) {
+    const { name, address, phone, email } = contact;
+    db.create({
+      index: indexName,
+      type: documentType,
+      id: name,
+      body: {
+        address: address,
+        phone: phone,
+        email: email
+      }
+    });
+  },
+  updateContact: function(name) {},
+  deleteContact: function(name) {}
+};
