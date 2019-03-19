ded9489464b2c9d659a847e3027262fbf7095494
Sam Pop <poplovitch.samuel@gmail.com>
2019-03-19 13:41:19 -0400

    controller methods update + api routing

--- controllers/addressBookController.js
+++ controllers/addressBookController.js
@@ -5,38 +5,80 @@ const INDEX = process.env.INDEX || "addressbook";
 const TYPE_DOCUMENT = process.env.TYPE_DOCUMENT || "contact";
 
 module.exports = {
-  getAllContacts: function(pageSize, pageOffset, query) {},
-  getContact: function(name) {
-    return db.get({
+  getAllContacts: async function(pageSize, page) {
+    const from = (page - 1) * pageSize;
+    return await db.search({
       index: INDEX,
       type: TYPE_DOCUMENT,
-      id: name
+      body: {
+        size: pageSize,
+        from: from,
+        query: {
+          match_all: {}
+        }
+      }
     });
   },
-  addContact: function(contact) {
-    // const { name, address, phone, email } = contact;
-    const { name } = contact;
-    return db.create({
+  getContactByQuery: async function(pageSize, page, query) {
+    const from = (page - 1) * pageSize;
+    return await db.search({
       index: INDEX,
       type: TYPE_DOCUMENT,
-      id: name,
-      body: contact
+      body: {
+        size: pageSize,
+        from: from,
+        query: {
+          match: { name: query }
+        }
+      }
     });
   },
-  updateContact: function(name, contact) {
-    const contactToAdd = new Contact(contact);
-    return db.update({
+  getContactByName: async function(name) {
+    return await db.search({
       index: INDEX,
       type: TYPE_DOCUMENT,
-      id: name,
-      body: { ...contactToAdd, doc: { title: "Updated" } }
+      body: {
+        query: {
+          match: { name: name }
+        }
+      }
     });
   },
-  deleteContact: function(name) {
-    return db.delete({
+  addContact: async function(contact) {
+    const { name, address, phone, email } = contact;
+    return await db.index({
       index: INDEX,
       type: TYPE_DOCUMENT,
-      id: name
+      body: {
+        name: name,
+        address: address,
+        phone: phone,
+        email: email
+      }
+    });
+  },
+  updateContact: async function(qName, contact) {
+    const { name, address, phone, email } = contact;
+    return await db.updateByQuery({
+      index: INDEX,
+      type: TYPE_DOCUMENT,
+      body: {
+        query: {
+          match: { name: qName }
+        },
+        script: `ctx._source.name = '${name}'; ctx._source.address = '${address}'; ctx._source.phone = '${phone}'; ctx._source.email = '${email}';`
+      }
+    });
+  },
+  deleteContact: async function(name) {
+    return await db.deleteByQuery({
+      index: INDEX,
+      type: TYPE_DOCUMENT,
+      body: {
+        query: {
+          match: { name: name }
+        }
+      }
     });
   }
 };
--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,59 @@ const router = require("express").Router();
 const { check, validationResult } = require("express-validator/check");
 const addressBookController = require("../controllers");
 
-// TODO map indices
-router.get("/contact", (req, res) => {
-  if (req.query) console.log(req.query);
-});
+// GET
+router
+  // TODO: FINISH THIS ROUTE
+  // http://localhost:3001/contact?pageSize={}&page={}&query={}
+  .get("/contact", (req, res) => {
+    if (req.query) {
+      let { pageSize, page, query } = req.query;
 
+      // query = JSON.parse(req.query.query);
+      if (!pageSize || isNaN(pageSize)) pageSize = process.env.PAGE_SIZE || 10;
+      if (!page || isNaN(page)) page = process.env.PAGE || 1;
+      if (!query) {
+        addressBookController
+          .getAllContacts(pageSize, page)
+          .then(result => {
+            console.log(result);
+            if (result.hits.total > 0)
+              res.status(200).json(result.hits.hits.map(h => h._source));
+            else res.status(404).send("No contacts");
+          })
+          .catch(err => {
+            res.status(400).json(err.message);
+          });
+      } else {
+        addressBookController
+          .getContactByQuery(pageSize, page, query)
+          .then(result => {
+            console.log(result);
+            if (result.hits.total > 0)
+              res.status(200).json(result.hits.hits.map(h => h._source));
+            else res.status(404).send("No contacts");
+          })
+          .catch(err => {
+            res.status(400).json(err.message);
+          });
+      }
+    }
+  })
+  // get contact by name
+  .get("/contact/:name", (req, res) => {
+    addressBookController
+      .getContactByName(req.params.name)
+      .then(result => {
+        if (result.hits.total > 0)
+          res.status(200).json(result.hits.hits.map(h => h._source));
+        else res.status(404).json(`${req.params.name} could not be found.`);
+      })
+      .catch(err => {
+        res.status(400).json(err.message);
+      });
+  });
+
+// POST
 router.post(
   "/contact",
   [
@@ -29,33 +77,67 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-    addressBookController.addContact(req.body);
+    addressBookController
+      .addContact(req.body)
+      .then(() => res.status(201).json(`${req.body.name} was added!`))
+      .catch(err => {
+        res.status(400).json(err.message);
+      });
   }
 );
 
+// PUT
 router
-  //get contact by name
-  .get("/contact/:name", (req, res) => {
-    addressBookController
-      .getContact(req.params.name)
-      .then(result => {
-        if (result.found) res.json(result);
-      })
-      .catch(err => res.json(err.message));
-  })
   // update contact by name
-  .put("/contact/:name", (req, res) => {
-    addressBookController.updateContact(req.params.name, req.body);
-  })
+  .put(
+    "/contact/:name",
+    [
+      check("name")
+        .exists({ checkNull: true })
+        .isLength({ min: 2 }),
+      check("address")
+        .optional()
+        .isLength({ max: 200 }),
+      check("phone")
+        .optional()
+        .isInt()
+        .isLength({ max: 10 }),
+      check("email")
+        .optional()
+        .isEmail()
+    ],
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+      }
+      addressBookController
+        .updateContact(req.params.name, req.body)
+        .then(result => {
+          if (result.updated > 0)
+            res.status(200).json(`${req.body.name} was updated!`);
+          else res.status(404).json(`${req.params.name} does not exist`);
+        })
+        .catch(err => {
+          res.status(400).json(err.message);
+        });
+    }
+  );
+
+// DELETE
+router
   // delete contact by name
   .delete("/contact/:name", (req, res) => {
     addressBookController
       .deleteContact(req.params.name)
       .then(result => {
-        if (result.result === "deleted") res.json(result.result);
+        if (result.deleted === 0)
+          res.status(404).json(`${req.params.name} does not exist`);
+        if (result.deleted === 1)
+          res.status(200).json(`${req.params.name} has been deleted!`);
       })
       .catch(err => {
-        res.json(err.message);
+        res.status(400).json(err.message);
       });
   });
 
